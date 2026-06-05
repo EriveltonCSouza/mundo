71504b3e57a994a0b186c00b47bd9ee38818315d
EriveltonSouza <eriveltonecs@gmail.com>
2026-06-05 11:44:12 +0000

    novo arquivo

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import gif from './assets/love-i-love-you-gif.gif';
+///import gif from '..src/love-you.gif';
 
 function Home ( ){
 
@@ -6,12 +6,14 @@ function Home ( ){
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
 
         <h1>Renata Souza, você é uma vencedora, te amo!! Bjs</h1>
+        
         <img
-            src={gif}
-            alt="Mensagem de amor"
-            style ={{ maxWidth: '100%', borderRadius: '10px' }}
-            />
-        </div>
+              
+            src='/love-you.gif'
+            alt="Love You"
+            style ={{ maxWidth: '100%', borderRadius: '20px' }}
+        />
+    </div>
     );
 }
 export  default Home;
